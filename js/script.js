e72d52635985fe089256d1b835c4c7f308aa92fb
Tarmana <31622729+Tarmana@users.noreply.github.com>
2023-05-31 22:40:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Typing Script JS
 var typed = new Typed(".typing",{
-    strings: ["Web Designer", "Web Developer", "Youtuber", "Blogger"],
+    strings: ["Digital Marketer", "Affiliate Marketer", "Youtuber", "Blogger"],
     typeSpeed: 80,
     backSpeed: 80,
 });
 
 var typed = new Typed(".typing-2",{
-    strings: ["Web Developer", "Web Designer", "Youtuber", "Blogger"],
+    strings: ["Digital Marketer", "Affiliate Marketer", "Youtuber", "Blogger"],
     typeSpeed: 100,
     backSpeed: 60,
 });
